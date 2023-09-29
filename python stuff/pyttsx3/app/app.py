34e78600fe2d8f0ad07545f728ca8ea172244a52
Thor Christiansen <thorchr96@gmail.com>
2023-09-29 22:58:06 +0200

    works now, but sounds robotic.

--- python stuff/pyttsx3/app/app.py
+++ python stuff/pyttsx3/app/app.py
@@ -1,56 +1,52 @@
-from flask import Flask, request, send_file, jsonify
-import pyttsx3
+from flask import Flask, request, Response
+import subprocess
 import os
-import logging
 
-logging.basicConfig(filename='app.log', level=logging.DEBUG)
-   
 app = Flask(__name__)
 
 @app.route('/text-to-speech', methods=['POST'])
 def text_to_speech():
-    try:
-        text = request.form.get('text', '')
-        if not text:
-            return {'error': 'No text provided'}, 400
-        #text = request.json.get('text', '')
-        filename = '/output.mp3'
-        engine = pyttsx3.init()
-        engine.save_to_file(text, filename)
-        engine.runAndWait()
-        
-        return send_file(filename, as_attachment=True)
-    except:
-        error_msg = "Completely failed.\nReceived following payload:\n\n"
-        if request.json:
-            error_msg += request.json.get('text', '')
-        else:
-            error_msg += "No JSON data or 'text' key found in the payload."
+    text = request.json.get('text', '')
+    voice = request.json.get('voice', 'en-us')
+    speed = request.json.get('speed', 150)
+    pitch = request.json.get('pitch', 50)
 
-        app.logger.critical(error_msg)
+    if not text:
+        return {'error': 'No text provided'}, 400
 
-@app.route('/text-to-speech-get', methods=['GET'])
-def text_to_speech_get():
-    try:
-        text = request.args.get('text', '')  # Access 'text' from query parameters
-        if not text:
-            return {'error': 'No text provided'}, 400
-        filename = '/output.mp3'
-        engine = pyttsx3.init()
-        engine.save_to_file(text, filename)
-        engine.runAndWait()
-        
-        return send_file(filename, as_attachment=True)
-    except Exception as e:  # Catch and log exceptions
-        error_msg = f"Completely failed.\nReceived following payload:\n\n{request.args.get('text', '')}"
-        app.logger.critical(error_msg)
-        
-        # Include error details in the response
-        error_details = {
-            'error': 'An error occurred',
-            'error_message': str(e)  # Convert the exception to a string
-        }
-        return jsonify(error_details), 500
+    # Ensure the directory exists
+    if not os.path.exists("/app/audio_files"):
+        os.makedirs("/app/audio_files")
+
+    # Generate WAV file using espeak with MBROLA voice
+    wav_file = "/app/audio_files/output.wav"
+    subprocess.call(["espeak", "-w", wav_file, "-v", voice, "-s", str(speed), "-p", str(pitch), text])
+
+    if not os.path.exists(wav_file):
+        return {"error": "WAV file was not generated. Check espeak logs for more details."}, 500
+
+    # Convert WAV to MP3 using ffmpeg
+    mp3_file = "/app/audio_files/output.mp3"
+    process = subprocess.Popen(["ffmpeg", "-i", wav_file, mp3_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    # Log ffmpeg output for debugging
+    print("FFMPEG STDOUT:", stdout.decode())
+    print("FFMPEG STDERR:", stderr.decode())
+
+    # Check if the MP3 file exists before attempting to open it
+    if os.path.exists(mp3_file):
+        with open(mp3_file, 'rb') as f:
+            mp3_data = f.read()
+
+        # Optionally, remove temporary files
+        os.remove(wav_file)
+        os.remove(mp3_file)
+
+        return Response(mp3_data, content_type='audio/mpeg')
+
+    else:
+        return {"error": "MP3 file was not generated. Check ffmpeg logs for more details."}, 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
